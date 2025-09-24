99151c2bc46ea29c7dc521c13fc575c66166d512
mramzan2 <ramzan.malik@gmail.com>
2025-09-24 22:42:48 +0400

    price_curve_generator.py

--- price_curve_generator.py
+++ price_curve_generator.py
@@ -6,21 +6,21 @@ from datetime import datetime
 
 ## PYTHON LOOP
 
-def random_number(num):
+def random_price(num):
     return [round(random.uniform(0,100),3) for i in range(num)]
-list_loop = random_number(100)
-print('loop_number:',random_number(100))
 start_time_1= datetime.now()
+price_curve = random_price(100)
+print('price_curve:',random_price(100))
 end_time_1 = datetime.now()
 print("Loop time:", end_time_1 - start_time_1)
 
 ## NUMPY
 
-def random_number(num):
+def random_price(num):
     return np.round(np.random.uniform(0,100,num),3)
-list_numpy = random_number(100)
-print('numpy_number:',random_number(100))
 start_time_2 = datetime.now()
+price_curve = random_price(100)
+print('price_curve:',random_price(100))
 end_time_2 = datetime.now()
 print("NumPy time:", end_time_2 - start_time_2)
 
