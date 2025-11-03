90b53c09b206867582c9b903dcab092fa0d83f8d
mramzan2 <ramzan.malik@gmail.com>
2025-11-03 22:14:41 +0400

    price_curve_generator.py

--- daylight_saving_time.py
+++ daylight_saving_time.py
@@ -1,23 +1,53 @@
-from datetime import datetime
+from datetime import datetime, date
 
-year = 2025
-march_days = range(1, 32)
+def settlement_periods(date_str, granularity, location):
 
-sundays_in_march = []
+    dt = datetime.strptime(date_str, "%Y-%m-%d").date()
 
-for d in march_days:
-    date = datetime(year, 3, d)
-    if date.weekday() == 6:
-        sundays_in_march.append(d)
 
-last_sunday_march = max(sundays_in_march)
-oct_days = range(1, 32)
-sundays_in_oct = []
+    hourly = 24
+    half_hourly = 48
 
-for d in oct_days:
-    date = datetime(year, 10, d)
-    if date.weekday() == 6:
-        sundays_in_oct.append(d)
-last_sunday_oct = max(sundays_in_oct)
 
+    if location.lower() == "uk":
+        year = dt.year
+
+        sundays_in_march = []
+        for d in range(1, 32):
+            try:
+                check_date = datetime(year, 3, d).date()
+                if check_date.weekday() == 6:
+                    sundays_in_march.append(d)
+            except:
+                pass
+        last_sunday_march = max(sundays_in_march)
+
+
+        sundays_in_october = []
+        for d in range(1, 32):
+            try:
+                check_date = datetime(year, 10, d).date()
+                if check_date.weekday() == 6:
+                    sundays_in_october.append(d)
+            except:
+                pass
+        last_sunday_october = max(sundays_in_october)
+
+
+        if dt == datetime(year, 3, last_sunday_march).date():
+
+            hourly = 23
+            half_hourly = 46
+        elif dt == datetime(year, 10, last_sunday_october).date():
+
+            hourly = 25
+            half_hourly = 50
+
+
+    if granularity.lower() in ["h", "hourly"]:
+        return hourly
+    elif granularity.lower() in ["hh", "half-hourly"]:
+        return half_hourly
+    else:
+        raise ValueError("Granularity must be 'hourly' or 'half-hourly'")
 
